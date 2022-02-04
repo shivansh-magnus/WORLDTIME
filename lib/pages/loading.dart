8e7f48949099e3d069474d1f0f5f75a509edae09
Shivansh <Dwivedi>
2022-02-05 02:49:16 +0530

    Getting getData function into class World time and then importing that file to show Date and time on screen

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -8,32 +9,31 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getTime() async {
-    Response response = await get(
-        Uri.parse('http://worldtimeapi.org/api/timezone/Europe/London'));
-    Map data = jsonDecode(response.body);
-    //print(data);
+  String time = 'loading';
 
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-
-    //print('$datetime - $offset');
-
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
+  void setUpWorldTime() async {
+    WorldTime instance = WorldTime(
+        Location: 'Berlin', flag: 'germany.png', url: '/Europe/berlin');
+    await instance.getTime();
+    //print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    getTime();
+    setUpWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
