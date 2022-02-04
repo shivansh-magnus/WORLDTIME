35ad89bf3b465b71e56702102ef3b86fbf734247
Shivansh <Dwivedi>
2022-02-05 02:10:24 +0530

    Adding world time API to loading route

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,6 +8,28 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getTime() async {
+    Response response = await get(
+        Uri.parse('http://worldtimeapi.org/api/timezone/Europe/London'));
+    Map data = jsonDecode(response.body);
+    //print(data);
+
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+
+    //print('$datetime - $offset');
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
